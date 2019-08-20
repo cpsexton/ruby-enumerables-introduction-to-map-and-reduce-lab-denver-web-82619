af4828fe559117bd1defb142d8443f5574d0bf41
Christopher Sexton <cpsexton@gmail.com>
2019-08-20 16:48:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,8 @@ def reduce_to_total(source_array, starting_point = 0)
     while i < source_array.length do
     total += source_array[i]
     i += 1 
-end
+    end
+end    
 
 def reduce_to_all_true(source_array)
   source_array.all?
