be13d6da94504cd43e6dc5244097a9c80c9d451c
Christopher Sexton <cpsexton@gmail.com>
2019-08-20 16:51:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,11 +40,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, i = 0)
-  total = i;
-  counter = 0;
+  total = i
+  counter = 0
   while counter < source_array.length do
-    total += source_array[counter];
-    counter = counter + 1;
+    total += source_array[counter]
+    counter = counter + 1
   end
   return total
 end   
