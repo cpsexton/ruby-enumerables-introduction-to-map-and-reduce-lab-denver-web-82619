fa1819d3be1a778a9dfbd3dc960b6fee2e3e74ae
Christopher Sexton <cpsexton@gmail.com>
2019-08-20 16:42:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,9 +39,13 @@ def map_to_square(source_array)
   return new
 end
 
+#def reduce_to_total(source_array, starting_point)
+#  source_array.reduce(starting_point) { |sum, num| sum + num #}
+#end
+
 def reduce_to_total(source_array, starting_point)
-  source_array.reduce(starting_point) { |sum, num| sum + num }
-end
+  source_array.reduce(:+)
+end  
 
 def reduce_to_all_true(source_array)
   source_array.all?
